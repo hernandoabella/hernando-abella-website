9d3316e37330ba6c0831b4aaf3d94409f6c05c8a
Hernando <hernandoabella@gmail.com>
2021-02-08 10:35:13 -0500

    changes on buttons

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const menuIcon = document.querySelector('.hamburguesa');
 const navbar = document.querySelector('.navigation__bar');
+const icons = document.querySelector('.icons');
 
 menuIcon.addEventListener('click', ()=>{
     menuIcon.classList.toggle('change');
     navbar.classList.toggle('change');
+    icons.classList.toggle('change');
 });
