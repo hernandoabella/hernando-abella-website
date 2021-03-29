116cc4d06a0c6546b47ebaf0bb355ec01a72bf2b
Hernando Abella <hernandoabella@gmail.com>
2021-03-29 09:54:01 -0500

    adding more features

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
