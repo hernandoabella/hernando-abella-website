d97a4898c3839839cb4e0475e0af7fd3985393d7
Hernando Abella <hernandoabella@gmail.com>
2021-03-29 21:27:31 -0500

    adding copy clipboard

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ $(".hover").mouseleave(
       $(this).removeClass("hover");
     }
   );
+
+
+// Copy clipboard
+
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+  }
+  
+  function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
