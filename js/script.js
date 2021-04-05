ac9edba6cba10cc338ef0f5a40a8f24db40fdf66
Hernando Abella <hernandoabella@gmail.com>
2021-04-04 19:35:16 -0500

    addingm more staff editing webpage

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
