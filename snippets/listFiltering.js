5124f79978a5e8e90f3e5ff434dfa72f4c0d830a
Hernando Abella <hernandoabella@gmail.com>
2021-05-26 07:21:16 -0500

    adding new stuff

--- snippets/listFiltering.js
+++ snippets/listFiltering.js
@@ -1,31 +1,14 @@
-$(document).ready(function() {
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    showPage(1, value);
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
-
-  pageSize = 10;
-
-  showPage = function(page, searchTerm) {
-    let $filteredCards = $("li a");
-    if (searchTerm) {
-      $filteredCards = $filteredCards.filter(function() {
-        return $(this).text().toLowerCase().indexOf(searchTerm) > -1;
-      });
-    }
-    $("li a").parent().hide(); // in here add .parent()
-    $filteredCards.each(function(n) {
-      if (n >= pageSize * (page - 1) && n < pageSize * page)
-        $(this).parent().show(); // also in here
-    });
-  }
-
-  showPage(1);
-
-  $("#paginator a").click(function() {
-    $("#paginator a").removeClass("pagActive");
-    $(this).addClass("pagActive");
-    showPage(parseInt($(this).data('page')), $("#myInput").val().toLowerCase());
-  });
-
-});
+}
