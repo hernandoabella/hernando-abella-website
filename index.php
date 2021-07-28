22b9902dcc4bbb60ae96ef26a2af13ae405ae20e
Hernando Abella <hernandoabella@gmail.com>
2021-07-28 05:27:02 -0500

    stuff testing some php...

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <!-- <a href="consejos/"><i class="fas fa-lightbulb"></i> Consejos</a> -->
         <!-- <a href="algoritmos">Algoritmos</a> -->
         <!-- <a href="eq">Preguntas entrevista</a> -->
-        <a href="https://www.paypal.com/donate?hosted_button_id=LWK6VN665RSEL" target="_blank"><i class="fab fa-paypal"></i> Donación</a>
+        <a href="https://www.paypal.com/donate?hosted_button_id=LWK6VN665RSEL" target="_blank"><i class="fab fa-paypal"></i> Donar</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
           <i class="fa fa-bars"></i>
         </a>
@@ -291,10 +291,79 @@
 
       <h2>Contacto</h2>
 
-      <p>Para cualquier consulta puedes contactarme al correo electrónico: <b class="correo">hernandoabella@gmail.com</b></p>  
+      <?php
+
+$errors = [];
+$errorMessage = '';
+
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
+    }
+
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+
+
+    if (empty($errors)) {
+        $toEmail = 'hernandoabella@gmail.com';
+        $emailSubject = 'New email from your contant form';
+        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+
+        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+        $body = join(PHP_EOL, $bodyParagraphs);
+
+        if (mail($toEmail, $emailSubject, $body, $headers)) {
+            header('Location: thank-you.html');
+        } else {
+            $errorMessage = 'Oops, something went wrong. Please try again later';
+        }
+    } else {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    }
+}
+
+?>
+
+  <form action="/mail_form.php" method="post" id="contact-form">
+    <h2>Contact us</h2>
+
+    <?php echo((!empty($errorMessage)) ? $errorMessage : '') ?>
+    <p>
+      <label>First Name:</label>
+      <input name="name" type="text" value="dima"/>
+    </p>
+    <p>
+      <label>Email Address:</label>
+      <input style="cursor: pointer;" name="email" value="dima@dima.com" type="text"/>
+    </p>
+    <p>
+      <label>Message:</label>
+      <textarea name="message">dima</textarea>
+    </p>
+
+    <p>
+      <input type="submit" value="Send"/>
+    </p>
+  </form>
+  
+
+      <!-- <p>Para cualquier consulta puedes contactarme al correo electrónico: <b class="correo">hernandoabella@gmail.com</b></p>  
       <form action="https://formsubmit.co/hernandoabella@gmail.com" method="POST">
           <label for="nombre">Nombre Completo</label>
-          <input type="text" placeholder="Hernando Abella"><br><br>
+          <input type="text" name="name" placeholder="Hernando Abella"><br><br>
           <input type="hidden" name="_subject" value="¡Nuevo mensaje!">
           <input type="hidden" name="_cc" value="another@email.com">
           <label for="email">Correo Electrónico</label>
@@ -303,9 +372,9 @@
           <label for="number">Teléfono</label>
           <input type="tel" name="tel" placeholder="+573043932597"><br><br>
           <label for="mensaje">Mensaje</label>
-          <textarea name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea><br>
+          <textarea name="comment" id="" cols="30" rows="10" placeholder="Mensaje"></textarea><br>
           <button type="submit">Enviar</button>
-      </form>
+      </form> -->
      
     </section>
 
@@ -344,6 +413,46 @@
         </div>
 
     </footer>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
+    <script>
+        const constraints = {
+            name: {
+                presence: { allowEmpty: false }
+            },
+            email: {
+                presence: { allowEmpty: false },
+                email: true
+            },
+            message: {
+                presence: { allowEmpty: false }
+            }
+        };
+
+        const form = document.getElementById('contact-form');
+
+        form.addEventListener('submit', function (event) {
+            const formValues = {
+                name: form.elements.name.value,
+                email: form.elements.email.value,
+                message: form.elements.message.value
+            };
+
+            const errors = validate(formValues, constraints);
+
+            if (errors) {
+                event.preventDefault();
+                const errorMessage = Object
+                    .values(errors)
+                    .map(function (fieldValues) {
+                        return fieldValues.join(', ')
+                    })
+                    .join("\n");
+
+                alert(errorMessage);
+            }
+        }, false);
+    </script>
     
     <script src="./js/script.js"></script>
     
