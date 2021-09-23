18a5342bbc1f53a3248daaf0375791c8ea5d0197
Hernando Abella <hernandoabella@gmail.com>
2021-09-23 14:56:07 -0500

    agregando mejoras

--- js/script.js
+++ js/script.js
@@ -63,10 +63,10 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" activo", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " activo";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
