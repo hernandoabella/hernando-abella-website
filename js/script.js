b92c9659d4f15b868f90b140ac487460a7ca0a85
Hernando Abella <hernandoabella@gmail.com>
2021-11-04 16:46:36 -0500

    agragando efecto de tipado

--- js/script.js
+++ js/script.js
@@ -4,8 +4,64 @@ let toggleSwitch = document.querySelector('.slider');
 let burger = document.querySelector('.menu__hamburguesa');
 let panel = document.querySelector('.ul');
 
-
 // ________________ VARIABLES ________________  
+
+// ________________ EFECTO DE TIPADO ________________  
+
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+
+const textArray = ['Desarrollador Web', 'Músico', 'Youtuber'];
+const typingDelay = 150;
+const erasingDelay = 90;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if(!(cursorSpan.classList.contains('typing'))) {
+        cursorSpan.classList.add('typing');
+    }
+    // if last letter of a word is not typed
+    if(charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        cursorSpan.classList.remove('typing');
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if(!(cursorSpan.classList.contains('typing'))) {
+        cursorSpan.classList.add('typing');
+    }
+    // if string is not entirely erased
+    if(charIndex > 0) { 
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        cursorSpan.classList.remove('typing');
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) {
+            textArrayIndex = 0;
+        }
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    if(textArray.length) {
+        setTimeout(type, newTextDelay + 250);
+    }
+})
+
+// ________________ EFECTO DE TIPADO ________________  
+
 
 // ________________ STICK NAV ONSCROLL ________________  
 
