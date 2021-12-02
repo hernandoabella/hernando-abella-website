2a5d29ce334445b96c81d3545730e4ddf8dbce31
Hernando Abella <hernandoabella@gmail.com>
2021-12-02 12:20:25 -0500

    modificaciones necesarias

--- js/script.js
+++ js/script.js
@@ -94,39 +94,3 @@ burger.addEventListener('click', () => {
 
 // ________________ MENÚ HAMBURGUESA ________________ 
   
-// ________________ MODO OSCURO ________________  
-
-const modoOscuro = () => {
-  let elemento = document.body;
-  elemento.classList.toggle('modo-oscuro');
-}
-
-toggleSwitch.addEventListener('click', modoOscuro);
-
-
-// ________________ MODO OSCURO ________________  
-
-
-// ________________ DESLIZADOR ________________  
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" activo", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " activo";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
-
-// ________________ DESLIZADOR ________________  
