be6f5bb82f9a029cbae02582fdcf14a6c46a6851
Hernando Abella <hernandoabella@gmail.com>
2021-12-29 15:46:21 -0500

    bug de scroll indicator solucionado

--- aprende/git/main.js
+++ aprende/git/main.js
@@ -6,8 +6,6 @@ function closeNav() {
     document.getElementById("course__sidebar").style.transform = "translate(-300px)";
 }
 
-
-
 function darkSide(){
   var ladoOscuro, cuerpo;
 
@@ -22,12 +20,6 @@ function irAtras(){
   document.location.href = "../";
 }
 
-
-
-
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -39,7 +31,7 @@ function myFunction() {
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(); myFunction();}
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
