817e9cb6bfa25553c0cc8816e5d5da1268ea598b
Hernando Abella <hernandoabella@gmail.com>
2022-02-25 18:17:51 -0500

    mejoras absolutas en projectos

--- proyectos/contador/component.js
+++ proyectos/contador/component.js
@@ -1,10 +1,11 @@
 // botones para compartir proyectos
-let compartir = document.querySelector('.compartir');
-let copiarLink = document.querySelector('.copy-link');
-
-
-const showShareBox = () => {
- 
+const compartir = document.querySelector('.compartir');
+const copiarLink = document.querySelector('.copy-link');
+const cajaCompartir = document.querySelector('.share-box');
+const html = document.querySelector('html');
+const clsBtn = document.querySelector('.closebtn');
+const showShareBox = (e) => {
+  cajaCompartir.classList.toggle('transformActive');
 }
 
 const copiarEnlace = () => {
@@ -16,9 +17,18 @@ const copiarEnlace = () => {
 }
 
 const outFunc = () => {
-  copiarLink.innerHTML = 'Copiar enlace';
+  copiarLink.innerHTML = '<i class="fa fa-clipboard"></i> ' + 'Copiar enlace';
+}
+
+window.onclick = function(e) {
+  if(e.target == cajaCompartir) {
+    cajaCompartir.classList.toggle('transformActive');
+  }
 }
 
 compartir.addEventListener('click', showShareBox);
 copiarLink.addEventListener('click', copiarEnlace);
 copiarLink.addEventListener('mouseout', outFunc);
+clsBtn.addEventListener('click', function(){
+  cajaCompartir.classList.toggle('transformActive');
+})
