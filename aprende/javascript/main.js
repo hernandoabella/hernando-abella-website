bc52cde2c8dfaad2b485ad034fc635d34e560b33
Hernando Abella <hernandoabella@gmail.com>
2022-03-11 16:41:06 -0500

    quite barra lateral de la seccion aprende

--- aprende/javascript/main.js
+++ aprende/javascript/main.js
@@ -1,11 +1,14 @@
+// abre la navegacion lateral
 function openNav() {
     document.getElementById("course__sidebar").style.transform = "translate(0px)";
 }
 
+// cierra la navegacion lateral izquierda
 function closeNav() {
     document.getElementById("course__sidebar").style.transform = "translate(-300px)";
 }
 
+// modo oscuro
 function darkSide(){
   var ladoOscuro, cuerpo;
 
@@ -16,6 +19,7 @@ function darkSide(){
   cuerpo.classList.toggle('dark');
 }
 
+// ir atras
 function irAtras(){
   document.location.href = "../";
 }
@@ -57,14 +61,9 @@ var panel = document.querySelector('.panel');
 var i;
 var a = document.getElementsByTagName('a');
 
-
 for (e = 0; e < acc.length; e++) {
   acc[e].addEventListener("click", function() {
     this.classList.toggle("active");
-    for (j = 0; j < acc.length; j++) {
-      if (j !== i)
-        acc[j].nextElementSibling.style.maxHeight = null;
-    }
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
@@ -82,6 +81,7 @@ for (let i = 0; i < a.length; i++){
     acc[i].classList.toggle('active');
     for (j = 0; j < acc.length; j++) {
       if (j !== i)
+        acc[j].classList.remove('active');
         acc[j].nextElementSibling.style.maxHeight = null;
     }
     if (panelDOM[i].style.maxHeight) {
@@ -91,4 +91,23 @@ for (let i = 0; i < a.length; i++){
     }
     
   });
+}
+
+
+// busqueda por filtro
+
+function buscarNav(){
+  var entrada, filtro, anchor, v, valorText;
+  entrada = document.getElementById('miEntrada');
+  filtro = entrada.value.toUpperCase();
+  anchor = document.getElementsByTagName('a');
+
+  for(v = 0; v < anchor.length; v++){
+    valorText = anchor[v].textContent || anchor[v].innerText;
+    if(valorText.toUpperCase().indexOf(filtro) > -1){
+      anchor[v].style.display = "";
+    }else{
+      anchor[v].style.display = "none";
+    }
+  }
 }
