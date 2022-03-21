c87b324530bb8b336d15bd41d64cb90ecdd0f73f
Hernando Abella <hernandoabella@gmail.com>
2022-03-21 14:34:02 -0500

    mejorando codigo js

--- aprende/javascript/main.js
+++ aprende/javascript/main.js
@@ -21,39 +21,6 @@ function darkSide(){
 
 // cambiar icono de modo oscuro al hacer clic
 
-function cambiarIcono(x) {
-  x.classList.toggle("fa-thumbs-down");
-}
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-
-// Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction(); myFunction();}
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-// Filtrar lista
-
 
 // Acordión
 var acc = document.getElementsByClassName("accordion");
