f584f93d51e83500fb61c41de18c7ebc470dcff2
Hernando Abella <hernandoabella@gmail.com>
2024-05-14 01:51:56 -0500

    Update Resources.tsx

---
+++ components/Resources.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Resources = () => {
+  return (
+    <div>
+      <h2 className="mb-5 text-2xl font-semibold">Resources</h2>
+    </div>
+  );
+};
+
+export default Resources;
