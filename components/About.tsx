73e694f173adfb7c8e224357922facb06c631957
Hernando Abella <hernandoabella@gmail.com>
2024-05-14 00:32:15 -0500

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -17,10 +17,30 @@ function About() {
     // Add more skills as needed
   ];
 
+  // Define an array of passions and interests
+  const passionsAndInterests = [
+    "Web development",
+    "Mobile app development",
+    "Open-source projects",
+    "Problem-solving",
+    "Learning new technologies",
+    // Add more passions and interests as needed
+  ];
+
+  // Define an array of soft skills
+  const softSkills = [
+    "Communication",
+    "Teamwork",
+    "Adaptability",
+    "Problem-solving",
+    "Creativity",
+    // Add more soft skills as needed
+  ];
+
   return (
     <div>
-      <h2 className="text-xl font-semibold mb-3">About me</h2>
-      <p>
+      <h2 className="mb-5 text-2xl font-semibold">About Me</h2>
+      <p className="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-lg p-4 mb-6">
         I love empowering individuals to learn new technologies effortlessly,
         fostering growth and success in their tech journey.
       </p>
@@ -28,11 +48,46 @@ function About() {
       {/* Skills Section */}
       <div className="mt-8">
         <h3 className="text-xl font-semibold mb-3">Skills</h3>
-        <ul className="flex flex-wrap gap-3 text-gray-600 dark:text-gray-300 list-inside">
+        <div className="flex flex-wrap gap-2">
           {skills.map((skill, index) => (
-            <li key={index}>{skill}</li>
+            <div
+              key={index}
+              className="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-lg p-2"
+            >
+              {skill}
+            </div>
           ))}
-        </ul>
+        </div>
+      </div>
+
+      {/* Passion and Interests Section */}
+      <div className="mt-8">
+        <h3 className="text-xl font-semibold mb-3">Passion and Interests</h3>
+        <div className="flex flex-wrap gap-2">
+          {passionsAndInterests.map((interest, index) => (
+            <div
+              key={index}
+              className="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-lg p-2"
+            >
+              {interest}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Soft Skills Section */}
+      <div className="mt-8">
+        <h3 className="text-xl font-semibold mb-3">Soft Skills</h3>
+        <div className="flex flex-wrap gap-2">
+          {softSkills.map((softSkill, index) => (
+            <div
+              key={index}
+              className="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-lg p-2"
+            >
+              {softSkill}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
