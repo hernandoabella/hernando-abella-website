32dd0c41a39e9759e82088d416258db984611100
Hernando Abella <hernandoabella@gmail.com>
2024-05-14 01:51:54 -0500

    Update Contact.tsx

---
+++ components/Contact.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2 className="mb-5 text-2xl font-semibold">Contact</h2>
+    </div>
+  );
+};
+
+export default Contact;
