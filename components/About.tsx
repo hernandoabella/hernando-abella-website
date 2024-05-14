7f85ad6a2afc4544c7b4449b45e54a736e1d67d7
Hernando Abella <hernandoabella@gmail.com>
2024-05-14 00:22:24 -0500

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 
 function About() {
+  // Define an array of skills
+  const skills = [
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Express.js",
+    "HTML",
+    "CSS",
+    "MongoDB",
+    "SQL",
+    "Git",
+    "AWS",
+    "Docker",
+    // Add more skills as needed
+  ];
+
   return (
     <div>
-      <h2 className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">About me</h2>
+      <h2 className="text-xl font-semibold mb-3">About me</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-        temporibus quos explicabo similique fugit distinctio eaque nostrum!
-        Quibusdam magni beatae, modi expedita aliquam laboriosam error, dicta
-        magnam explicabo, fugiat iure.
+        I love empowering individuals to learn new technologies effortlessly,
+        fostering growth and success in their tech journey.
       </p>
+
+      {/* Skills Section */}
+      <div className="mt-8">
+        <h3 className="text-xl font-semibold mb-3">Skills</h3>
+        <ul className="flex flex-wrap gap-3 text-gray-600 dark:text-gray-300 list-inside">
+          {skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
