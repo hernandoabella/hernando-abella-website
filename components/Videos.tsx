8c0cc96b560abf534a4380938f4d85b2b1415ad4
Hernando Abella <hernandoabella@gmail.com>
2024-05-14 01:51:58 -0500

    Update Videos.tsx

---
+++ components/Videos.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Videos = () => {
+  return (
+    <div>
+      <h2 className="mb-5 text-2xl font-semibold">Videos</h2>
+    </div>
+  );
+};
+
+export default Videos;
