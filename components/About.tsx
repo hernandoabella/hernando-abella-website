76c0eafd37be553dba1d1397c64030669baa0b6c
Hernando Abella <hernandoabella@gmail.com>
2024-05-13 23:41:11 -0500

    Create About.tsx

---
+++ components/About.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2 className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">About me</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+        temporibus quos explicabo similique fugit distinctio eaque nostrum!
+        Quibusdam magni beatae, modi expedita aliquam laboriosam error, dicta
+        magnam explicabo, fugiat iure.
+      </p>
+    </div>
+  );
+}
+
+export default About;
