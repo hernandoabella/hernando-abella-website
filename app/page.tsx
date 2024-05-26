3c26dcdf75fa71d7e28496056d763d66f2617927
Hernando Abella <hernandoabella@gmail.com>
2024-05-25 19:16:06 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,9 @@
-"use client";
-
-import { useState } from "react";
 import Hero from "@/app/components/Hero";
-import About from "@/app/components/About";
-import Books from "@/app/components/Books";
-import Resources from "@/app/components/Resources";
-import SideProjects from "@/app/components/SideProjects";
-import Videos from "@/app/components/Videos";
-import Contact from "@/app/components/Contact";
 import Footer from "@/app/components/Footer";
-import { Button } from "@/app/components/ui/button";
 import DarkMode from "@/app/components/DarkMode";
+import { Button } from "@/app/components/ui/button";
 
 export default function Home() {
-  const [activeContent, setActiveContent] = useState("about");
-
-  const buttonData = [
-    { key: "about", label: "About me" },
-    { key: "books", label: "Books" },
-    { key: "resources", label: "Resources" },
-    { key: "sideProjects", label: "Side Projects" },
-    { key: "videos", label: "Videos" },
-    { key: "contact", label: "Contact" },
-  ];
-
-  const renderContent = () => {
-    switch (activeContent) {
-      case "about":
-        return <About />;
-      case "books":
-        return <Books />;
-      case "resources":
-        return <Resources />;
-      case "sideProjects":
-        return <SideProjects />;
-      case "videos":
-        return <Videos />;
-      case "contact":
-        return <Contact />;
-      default:
-        return null;
-    }
-  };
-
   return (
     <main className="p-5 md:p-10 flex flex-col md:flex-row items-center justify-center md:h-screen">
       <div className="absolute top-10 right-10">
@@ -51,19 +12,14 @@ export default function Home() {
       <div className="w-full max-w-screen-md mt-5 md:ml-5 md:w-1/2">
         <Hero />
         <div className="flex flex-wrap gap-5">
-          {buttonData.map((button) => (
-            <Button
-              key={button.key}
-              onClick={() => setActiveContent(button.key)}
-              className={activeContent === button.key ? "active-btn" : ""}
-            >
-              {button.label}
-            </Button>
-          ))}
+          <Button>About Me</Button>
+          <Button>About Me</Button>
+          <Button>About Me</Button>
+          <Button>About Me</Button>
+          <Button>Contact</Button>
         </div>
         <Footer />
       </div>
-      <div className="mt-5 md:w-1/2">{renderContent()}</div>
     </main>
   );
 }
