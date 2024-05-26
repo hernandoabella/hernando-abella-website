e60b81747c9a3430c53e4bb36570f10f08a89c7b
Hernando Abella <hernandoabella@gmail.com>
2024-05-25 19:49:09 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import Footer from "@/app/components/Footer";
 import DarkMode from "@/app/components/DarkMode";
 import { Button } from "@/app/components/ui/button";
 import Link from "next/link";
+import {
+  FaBook,
+  FaYoutube,
+  FaEnvelope,
+  FaUser,
+  FaFolder,
+  FaRocket,
+} from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -10,15 +18,39 @@ export default function Home() {
       <div className="absolute top-10 right-10">
         <DarkMode />
       </div>
-      <div className="w-full max-w-screen-md mt-5 md:ml-5 md:w-1/2">
+      <div>
         <Hero />
         <div className="flex flex-wrap gap-5">
-          <Link href="https://github.com/hernandoabella/"><Button>About Me</Button></Link>
-          <Link href="https://github.com/hernandoabella/"><Button>Portfolio</Button></Link>
-          <Link href="https://github.com/hernandoabella/books"><Button>Books</Button></Link>
-          <Link href="https://github.com/hernandoabella/side-projects"><Button>Side Projects</Button></Link>
-          <Link href="https://github.com/hernandoabella/"><Button>Contact Me</Button></Link>
-
+          <Link href="https://github.com/hernandoabella/">
+            <Button>
+              <FaUser className="mr-2" /> About Me
+            </Button>
+          </Link>
+          <Link href="https://github.com/hernandoabella/">
+            <Button>
+              <FaFolder className="mr-2" /> Portfolio
+            </Button>
+          </Link>
+          <Link href="https://github.com/hernandoabella/books">
+            <Button>
+              <FaBook className="mr-2" /> Books
+            </Button>
+          </Link>
+          <Link href="https://github.com/hernandoabella/side-projects">
+            <Button>
+              <FaRocket className="mr-2" /> Side Projects
+            </Button>
+          </Link>
+          <Link href="https://youtube.com/c/hernandoabella">
+            <Button>
+              <FaYoutube className="mr-2" /> Videos
+            </Button>
+          </Link>
+          <Link href="mailto:hernandoabella@gmail.com">
+            <Button>
+              <FaEnvelope className="mr-2" /> Contact Me
+            </Button>
+          </Link>
         </div>
         <Footer />
       </div>
