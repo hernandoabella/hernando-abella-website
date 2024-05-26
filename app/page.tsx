9b6e83922540636993638381f60ff40403160300
Hernando Abella <hernandoabella@gmail.com>
2024-05-25 19:27:05 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/app/components/Hero";
 import Footer from "@/app/components/Footer";
 import DarkMode from "@/app/components/DarkMode";
 import { Button } from "@/app/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,11 +13,12 @@ export default function Home() {
       <div className="w-full max-w-screen-md mt-5 md:ml-5 md:w-1/2">
         <Hero />
         <div className="flex flex-wrap gap-5">
-          <Button>About Me</Button>
-          <Button>About Me</Button>
-          <Button>About Me</Button>
-          <Button>About Me</Button>
-          <Button>Contact</Button>
+          <Link href="https://github.com/hernandoabella/"><Button>About Me</Button></Link>
+          <Link href="https://github.com/hernandoabella/"><Button>Portfolio</Button></Link>
+          <Link href="https://github.com/hernandoabella/books"><Button>Books</Button></Link>
+          <Link href="https://github.com/hernandoabella/side-projects"><Button>Side Projects</Button></Link>
+          <Link href="https://github.com/hernandoabella/"><Button>Contact Me</Button></Link>
+
         </div>
         <Footer />
       </div>
