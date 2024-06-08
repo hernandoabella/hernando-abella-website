98721d2303c17a9ad09e11926163a142787b0a5b
Hernando Abella <hernandoabella@gmail.com>
2024-06-07 23:53:33 -0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -29,8 +29,8 @@ const Footer = () => {
               initial={{ opacity: 0, y: -20, scale: 0.8 }}
               animate={{ opacity: 1, y: 0, scale: 1 }}
               whileHover={{ scale: 1.2 }}
-              transition={{ delay, duration: 0.8 }}
-              className="text-3xl cursor-pointer"
+            
+              className="text-3xl cursor-pointer hover:text-orange-500"
             >
               <Component />
             </motion.div>
