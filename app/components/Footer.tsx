fb77881f2882999ccceae20052abf47385ebfb21
Hernando Abella <hernandoabella@gmail.com>
2024-06-07 23:46:34 -0500

    icons updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,35 +1,36 @@
 import Link from "next/link";
 import React from "react";
 import {
-  FaTwitter,
-  FaInstagram,
-  FaYoutube,
-  FaPinterest,
-  FaGithub,
-  FaLinkedin,
-} from "react-icons/fa6";
+  RiTwitterXFill,
+  RiInstagramFill,
+  RiYoutubeFill,
+  RiPinterestFill,
+  RiGithubFill,
+  RiLinkedinFill
+} from "react-icons/ri";
+
 
 const Footer = () => {
   return (
     <div>
       <footer className="pt-10 flex gap-5 text-center justify-center">
         <Link href="https://www.x.com/hernandoabella">
-          <FaTwitter className="text-3xl" />{" "}
+          <RiTwitterXFill className="text-3xl" />
         </Link>
         <Link href="https://www.instagram.com/hernandoabella">
-          <FaInstagram className="text-3xl" />
+          <RiInstagramFill className="text-3xl" />
         </Link>
         <Link href="https://www.youtube.com/c/hernandoabella">
-          <FaYoutube className="text-3xl" />
+          <RiYoutubeFill className="text-3xl" />
         </Link>
         <Link href="https://www.pinterest.com/hernandoabella">
-          <FaPinterest className="text-3xl" />
+          <RiPinterestFill className="text-3xl" />
         </Link>
         <Link href="https://www.github.com/hernandoabella">
-          <FaGithub className="text-3xl" />
+          <RiGithubFill className="text-3xl" />
         </Link>
         <Link href="https://www.linkedin.com/in/hernandoabella">
-          <FaLinkedin className="text-3xl" />
+          <RiLinkedinFill className="text-3xl" />
         </Link>
       </footer>
     </div>
