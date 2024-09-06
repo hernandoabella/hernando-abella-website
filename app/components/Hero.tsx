179eca4fcfdd6f1cd96734ebcd83b6d58691da39
Hernando Abella <hernandoabella@gmail.com>
2024-09-05 23:23:01 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,22 +17,22 @@ const Hero = () => {
           />
         </div>
         <div>
-          <h1 className="text-5xl font-extrabold dark:text-white">
+          <h1 className="text-3xl md:text-5xl font-extrabold dark:text-white text-center">
             👋 Hey there! <br /> I&apos;m{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-orange-400">
               Hernando.
             </span>{" "}
           </h1>
-          <h3 className="py-5 text-4xl font-bold dark:text-white">
+          <h3 className="py-5 text-2xl text-center md:text-4xl font-bold dark:text-white">
             👨‍💻{" "}
             <span className="underline underline-offset-3 decoration-8 decoration-orange-400 dark:decoration-orange-600">
               <ReactTyped
                 strings={[
                   "Creative",
-                  "Full-Stack Developer",
-                  "Front-end Developer",
-                  "Back-end Developer",
-                  "UI/UX Designer",
+                  "Full-Stack",
+                  "Front-end",
+                  "Back-end",
+                  "UI/UX",
                   "Tech Enthusiast",
                   "Artist",
                 ]}
