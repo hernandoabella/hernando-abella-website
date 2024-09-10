5e70afef25a1b5e426de1f99f016d29de649a8e2
Hernando Abella <hernandoabella@gmail.com>
2024-09-10 17:31:09 -0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -12,12 +12,24 @@ import {
 } from "react-icons/ri";
 
 const icons = [
-  { Component: RiTwitterXFill, href: "https://www.x.com/hernandoabella", delay: 0 },
-  { Component: RiInstagramFill, href: "https://www.instagram.com/hernandoabella", delay: 0.2 },
-  { Component: RiYoutubeFill, href: "https://www.youtube.com/c/hernandoabella", delay: 0.4 },
-  { Component: RiPinterestFill, href: "https://www.pinterest.com/hernandoabella", delay: 0.6 },
-  { Component: RiGithubFill, href: "https://www.github.com/hernandoabella", delay: 0.8 },
-  { Component: RiLinkedinFill, href: "https://www.linkedin.com/in/hernandoabella", delay: 1 },
+  { Component: RiTwitterXFill, href: "https://www.x.com/hernandoabella" },
+  {
+    Component: RiInstagramFill,
+    href: "https://www.instagram.com/hernandoabella",
+  },
+  {
+    Component: RiYoutubeFill,
+    href: "https://www.youtube.com/c/hernandoabella",
+  },
+  {
+    Component: RiPinterestFill,
+    href: "https://www.pinterest.com/hernandoabella",
+  },
+  { Component: RiGithubFill, href: "https://www.github.com/hernandoabella" },
+  {
+    Component: RiLinkedinFill,
+    href: "https://www.linkedin.com/in/hernandoabella",
+  },
   { Component: RiWhatsappFill, href: "https://wa.me/573043932597" },
 ];
 
@@ -25,13 +37,13 @@ const Footer = () => {
   return (
     <div>
       <footer className="pb-10 flex gap-5 text-center justify-center">
-        {icons.map(({ Component, href, delay }, index) => (
+        {icons.map(({ Component, href }, index) => (
           <Link key={index} href={href} passHref>
             <motion.div
               initial={{ opacity: 0, y: -20, scale: 0.8 }}
               animate={{ opacity: 1, y: 0, scale: 1 }}
               whileHover={{ scale: 1.2 }}
-              transition={{ delay, duration: 0.3 }}
+              transition={{ duration: 0.3 }}
               className="text-3xl cursor-pointer hover:text-orange-500"
             >
               <Component />
