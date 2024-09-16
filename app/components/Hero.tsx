9ea64f8612082373b24e362b0f55ed44dc8705b9
Hernando Abella <hernandoabella@gmail.com>
2024-09-15 23:59:05 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,8 +4,8 @@ import { ReactTyped } from "react-typed";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col md:flex-row items-center gap-10 mb-10">
+    <div className="flex justify-center mt-10">
+      <div className="flex flex-col md:flex-row items-center gap-10 mb-10 mt-10">
         <div className="rounded-full p-2 bg-gradient-to-r from-orange-400 to-orange-600">
           <Image
             className="rounded-full"
@@ -17,32 +17,25 @@ const Hero = () => {
           />
         </div>
         <div>
-          <h1 className="text-3xl md:text-5xl font-extrabold dark:text-white text-center">
-            👋 Hey there! <br /> I&apos;m{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-orange-400">
-              Hernando.
-            </span>{" "}
+          <h1 className="text-3xl md:text-6xl font-extrabold dark:text-white">
+            👋 Hey there! 
           </h1>
-          <h3 className="py-5 text-2xl  md:text-4xl font-bold dark:text-white">
+          <h2 className="text-2xl md:text-4xl font-extrabold mt-5">
             {" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-orange-400">
             <span className="decoration-8 decoration-orange-400 dark:decoration-orange-600">
               <ReactTyped
                 strings={[
-                  "旦~~",
-                  "Creative",
-                  "=^_^=",
-                  "Full-Stack",
-                  "(~o~)",
-                  "Front-end",
-                  "ˊ＿>ˋ",
-                  "Back-end",
-                  "(*´▽｀*)",
-                  "UI/UX",
-                  "(*￣m￣)",
-                  "Tech Enthusiast",
-                  "¯\_(ツ)_/¯",
-                  "Artist",
+                  "My name is Hernando!",
+                  "Full-Stack Developer",
+                  "I do Front-end",
                   "ノಠ益ಠ)ノ",
+                  "And Back-end",
+                  "(*´▽｀*)",
+                  "Also UI/UX...",
+                  "(*￣m￣)",
+                  "It works on my machine",
+                  "¯\\_(ツ)_/¯"
                 ]}
                 typeSpeed={60}
                 backSpeed={70}
@@ -50,7 +43,9 @@ const Hero = () => {
                 loop
               />
             </span>
-          </h3>
+            </span>{" "}
+          </h2>
+          
         </div>
       </div>
     </div>
