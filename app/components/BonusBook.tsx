1406608fb937e9cf807dfa64b7271333322b0232
Hernando Abella <hernandoabella@gmail.com>
2024-10-16 23:52:37 -0500

    Create BonusBook.tsx

---
+++ app/components/BonusBook.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import BonusBtn from "./BonusBtn";
+
+const DownloadBonusBook = () => {
+  return (
+    <section className=" dark:bg-[#141414]">
+      <div className="mx-auto text-center py-20">
+        <BonusBtn />
+      </div>
+    </section>
+  );
+};
+
+export default DownloadBonusBook;
