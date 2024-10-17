3c9f64c321893b3e1753196a7bcd1fcb9b0bdbe4
Hernando Abella <hernandoabella@gmail.com>
2024-10-16 23:52:52 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
+import Books from "./components/Books";
+import BonusBook from "./components/BonusBook";
+import CTA from "./components/CTA";
+import SideProjects from "./components/SideProjects";
+import Contact from "./components/Contact";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <div>
+        <Header />
+        <Hero />
+        <About />
+        <SideProjects />
+        <Services />
+        <Testimonials />
+        <CTA />
+        <Books />
+        <BonusBook />
+        <Contact />
+        <Newsletter />
+        <Footer />
+      </div>
     </main>
   );
 }
