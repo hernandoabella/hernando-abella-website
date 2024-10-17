a05457e26d743c3dbda21e9bceca632c29d9fae4
Hernando Abella <hernandoabella@gmail.com>
2024-10-16 23:52:43 -0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,57 +1,88 @@
-import Link from "next/link";
-import React from "react";
-import { motion } from "framer-motion";
-import {
-  RiTwitterXFill,
-  RiInstagramFill,
-  RiYoutubeFill,
-  RiPinterestFill,
-  RiGithubFill,
-  RiLinkedinFill,
-  RiWhatsappFill,
-} from "react-icons/ri";
+"use client";
 
-const icons = [
-  { Component: RiTwitterXFill, href: "https://www.x.com/hernandoabella" },
-  {
-    Component: RiInstagramFill,
-    href: "https://www.instagram.com/hernandoabella",
-  },
-  {
-    Component: RiYoutubeFill,
-    href: "https://www.youtube.com/c/hernandoabella",
-  },
-  {
-    Component: RiPinterestFill,
-    href: "https://www.pinterest.com/hernandoabella",
-  },
-  { Component: RiGithubFill, href: "https://www.github.com/hernandoabella" },
-  {
-    Component: RiLinkedinFill,
-    href: "https://www.linkedin.com/in/hernandoabella",
-  },
-  { Component: RiWhatsappFill, href: "https://wa.me/573043932597" },
-];
+import React from "react";
+import {
+  FaLinkedin,
+  FaGithub,
+  FaInstagram,
+  FaYoutube,
+  FaPinterest,
+  FaWhatsapp,
+} from "react-icons/fa"; 
+
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="pb-10 flex gap-5 text-center justify-center">
-        {icons.map(({ Component, href }, index) => (
-          <Link key={index} href={href} passHref>
-            <motion.div
-              initial={{ opacity: 0, y: -20, scale: 0.8 }}
-              animate={{ opacity: 1, y: 0, scale: 1 }}
-              whileHover={{ scale: 1.2 }}
-              transition={{ duration: 0.3 }}
-              className="text-3xl cursor-pointer hover:text-orange-500"
-            >
-              <Component />
-            </motion.div>
-          </Link>
-        ))}
-      </footer>
-    </div>
+    <footer className="py-10 text-center bg-[#141414]">
+      <div>
+        {/* Sección de redes sociales */}
+        <div className="my-10 flex justify-center space-x-6">
+          <a
+            href="https://twitter.com/hernandoabella"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-green-500"
+          >
+            <FaXTwitter className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.instagram.com/hernandoabella"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-green-500"
+          >
+            <FaInstagram className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.youtube.com/c/hernandoabella"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-green-500"
+          >
+            <FaYoutube className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.pinterest.com/hernandoabella"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-green-500"
+          >
+            <FaPinterest className="w-6 h-6" />
+          </a>
+          <a
+            href="https://github.com/hernandoabella"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-green-500"
+          >
+            <FaGithub className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/hernandoabella"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-green-500"
+          >
+            <FaLinkedin className="w-6 h-6" />
+          </a>
+          <a
+            href="https://wa.me/+573043932597"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-green-500"
+          >
+            <FaWhatsapp className="w-6 h-6" />
+          </a>
+        </div>
+        {/* Derechos reservados */}
+        <div>
+          <p className="text-md">
+            © {new Date().getFullYear()} Hernando Abella. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
